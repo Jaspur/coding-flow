ed37e349bc1552254fc742c38dba661835ddef74
Jasper Koers <jasper@coding.agency>
2025-03-17 16:06:37 +0100

    📝 docs(ControllerGenerator): update method documentation
    - enhance method comments to provide clearer descriptions
    - include details about parameters and expected behavior
    - improve overall clarity for better understanding of methods

--- src/Services/ControllerGenerator.php
+++ src/Services/ControllerGenerator.php
@@ -47,7 +47,9 @@ class ControllerGenerator
             ) {}
 
             /**
-             * All {$model}s
+             * Retrieve a list of {$model}s with optional pagination.
+             *
+             * This method retrieves all the {$model}s in the database.
              *
              * @return {$model}Collection
              */
@@ -57,7 +59,10 @@ class ControllerGenerator
             }
 
             /**
-             * Get {$model}:id
+             * Retrieve a single {$model}.
+             *
+             * This method fetches a {$model} based on the provided data, typically from a unique identifier or other criteria. 
+             * If no {$model} is found matching the provided data, the method may throw a not found exception.
              *
              * @param non-empty-string \$id
              * @return {$model}Resource
@@ -68,7 +73,10 @@ class ControllerGenerator
             }
 
             /**
-             * Store {$model}
+             * Create a new {$model} in the database.
+             *
+             * This method accepts the {$model} data and creates a new entry in the database. 
+             * It will validate the input before creating the record, and then return the newly created {$model}.
              *
              * @param {$model}StoreRequest \$request
              * @return {$model}Resource
@@ -81,7 +89,11 @@ class ControllerGenerator
             }
 
             /**
-             * Update {$model}:id
+              * Update an existing {$model} with new data.
+             *
+             * This method allows you to update an existing {$model} record. It accepts the new data, validates it, 
+             * and applies the changes to the corresponding {$model} record in the database. 
+             * If the {$model} doesn't exist, an exception will be thrown.
              *
              * @param {$model}UpdateRequest \$request
              * @param non-empty-string \$id
@@ -95,7 +107,11 @@ class ControllerGenerator
             }
 
             /**
-             * Delete {$model}:id
+              * Delete an existing {$model} from the database.
+             *
+             * This method deletes a {$model} from the database based on the provided data. 
+             * It will return a no-content HTTP response (204) to indicate that the deletion was successful, 
+             * with no additional data included in the response body.
              *
              * @param non-empty-string \$id
              * @return Response
