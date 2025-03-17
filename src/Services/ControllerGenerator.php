88d3c2e47609921cb71f6e0dcdef783e814e107d
Jasper Koers <jasper@coding.agency>
2025-03-17 15:33:06 +0100

    ✨ feat(ControllerGenerator): add specific request classes
    - replace generic request with specific store and update request classes
    - improve code clarity and maintainability by using dedicated request types

--- src/Services/ControllerGenerator.php
+++ src/Services/ControllerGenerator.php
@@ -34,7 +34,8 @@ class ControllerGenerator
         namespace App\Http\Controllers;
 
         use App\Services\\{$model}Service;
-        use App\Http\Requests\\{$model}Request;
+        use App\Http\Requests\\{$model}StoreRequest;
+        use App\Http\Requests\\{$model}UpdateRequest;
         use App\Http\Resources\\{$model}Resource;
         use Illuminate\Http\Response;
         use Illuminate\Http\Resources\Json\AnonymousResourceCollection as {$model}Collection;
